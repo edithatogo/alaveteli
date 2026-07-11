c58a3839dc140a54566ceba44c279d4d0d657578
Dylan Mordaunt (ISLHD) <Dylan.Mordaunt@health.nsw.gov.au>
2026-07-11 18:32:01 +1000

    test: stub controller client address

--- spec/controllers/api/v1/sustainability_controller_spec.rb
+++ spec/controllers/api/v1/sustainability_controller_spec.rb
@@ -75,7 +75,7 @@ RSpec.describe Api::V1::SustainabilityController, type: :controller do
     end
 
     it 'rejects an invalid client address without querying the limiter' do
-      allow(request).to receive(:remote_ip).and_return('not-an-ip')
+      allow(controller.request).to receive(:remote_ip).and_return('not-an-ip')
       expect(limiter).not_to receive(:rule)
       expect(limiter).not_to receive(:records)
 
