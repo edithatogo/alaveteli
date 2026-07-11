595dcfdb50bdcd2ce0dad573fdbf6e8c384e171a
Dylan Mordaunt (ISLHD) <Dylan.Mordaunt@health.nsw.gov.au>
2026-07-11 20:56:43 +1000

    fix: bound request ZIP cache components

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -372,10 +372,16 @@ class RequestController < ApplicationController
         # url_title is the unique lookup key used above; hash it before using
         # it in a filesystem path so the route value cannot add path segments.
         cache_key = Digest::SHA256.hexdigest(params[:url_title].to_s)
+        version_key = Digest::SHA256.hexdigest(
+          @info_request.last_update_hash.to_s
+        )
+        cache_file_suffix = safe_zip_cache_suffix(
+          @info_request.zip_cache_file_suffix(@user)
+        )
         cache_file_path = RequestZipCachePath.call(
           cache_key: cache_key,
-          last_update_hash: @info_request.last_update_hash,
-          cache_file_suffix: @info_request.zip_cache_file_suffix(@user)
+          last_update_hash: version_key,
+          cache_file_suffix: cache_file_suffix
         )
         unless File.exist?(cache_file_path)
           FileUtils.mkdir_p(File.dirname(cache_file_path))
@@ -388,6 +394,13 @@ class RequestController < ApplicationController
   end
 
   private
+
+  def safe_zip_cache_suffix(suffix)
+    return '_hidden' if suffix == '_hidden'
+    return '_requester_only' if suffix == '_requester_only'
+
+    ''
+  end
 
   def info_request_params
     params.require(:info_request).permit(:title, :public_body_id, :tag_string)
